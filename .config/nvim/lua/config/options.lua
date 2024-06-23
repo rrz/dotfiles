fc1b34f4fd157e1201159a162f0326e9d6b64748
Рамиль Закиров <ramil.zakirov>
2024-06-23 21:53:14 +0300

    added nvim configs

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,14 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options hereb
+local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require("go.format").goimports()
+  end,
+  group = format_sync_grp,
+})
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
